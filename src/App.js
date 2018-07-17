e6a0da7a6452e1f09965dcd75d11cfed82f2ba12
zsp <zhaoshaopeng@bjmantis.com>
2018-07-17 09:42:36 +0800

    2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
-import Test from './containers/Test'
-import './App.css';
+import {Switch,Router, Route,BrowserRouter} from 'react-router-dom';
+import createBrowserHistory  from 'history/createBrowserHistory'
+import User from './containers/test/User'
+import './App.less';
 export default class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Test/>
-            </div>
+            <Router history={createBrowserHistory()}>
+                <div style={{padding:10}}>
+                    <Switch>
+                        <Route path="/" component={User} ></Route>
+                    </Switch>
+                </div>
+            </Router>
         );
     }
 }
